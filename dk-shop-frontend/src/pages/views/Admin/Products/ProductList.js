3ad2b4bad58254236f6d99009c81d97adf8fd151
khoand10 <khoand1011@gmail.com>
2020-08-11 11:16:23 +0700

    add paging list product

--- dk-shop-frontend/src/pages/views/Admin/Products/ProductList.js
+++ dk-shop-frontend/src/pages/views/Admin/Products/ProductList.js
@@ -1,14 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import _ from "lodash";
 import { Button } from 'react-bootstrap';
 import { confirmAlert } from 'react-confirm-alert';
+import Pagination from 'react-js-pagination';
+import "bootstrap/dist/css/bootstrap.css";
+
+const LENGTH_PER_PAGE = 10;
+const PAGE_RANGER_DISPLAY = 5;
 
 const ProductsList = ({ products = {} , history, getProducts, getCategories, categories, removeProduct}) => {
+    
+    const [activePage, setactivePage] = useState(1);
+
     useEffect(() => {
         getCategories();
         getProducts();
-    }, []);
+    }, [getCategories, getProducts]);
     const addNew = () => {
         history.push("/admin/product/new-product");
     }
@@ -37,6 +45,27 @@ const ProductsList = ({ products = {} , history, getProducts, getCategories, cat
           });
     }
 
+    let pagination;
+    if (Object.keys(products).length <= LENGTH_PER_PAGE) {
+        pagination = null;
+    } else {
+        pagination = (
+            <Pagination
+                pageRangeDisplayed={PAGE_RANGER_DISPLAY}
+                activePage={activePage}
+                itemsCountPerPage={LENGTH_PER_PAGE}
+                totalItemsCount={Object.keys(products).length}
+                onChange={(pageNumber) => setactivePage(pageNumber)}
+                itemClass="page-item"
+                linkClass="page-link"
+            />
+        );
+    }
+    const listProduct = Object.keys(products).slice((activePage - 1) * LENGTH_PER_PAGE, activePage * LENGTH_PER_PAGE);
+    if (Object.keys(products) === 0 && Object.keys(listProduct).length !== 0) {
+        this.setState({activePage: activePage - 1});
+    }
+
     return (
         <div className='products'>
             <h1 className="h3 mb-2 text-gray-800">Products</h1>
@@ -60,7 +89,7 @@ const ProductsList = ({ products = {} , history, getProducts, getCategories, cat
                                 </tr>
                             </thead>
                             <tbody>
-                                {!_.isEmpty(products) && Object.keys(products).map((key, index) => (
+                                {listProduct.map((key, index) => (
                                     <tr key={index}>
                                         <td>{_.get(products[key], 'name', '')}</td>
                                         <td><img src={_.get(products[key], 'image', '')} alt="" width="50" /></td>
@@ -78,7 +107,7 @@ const ProductsList = ({ products = {} , history, getProducts, getCategories, cat
 
                             </tbody>
                         </table>
-
+                        {pagination}
                     </div>
                 </div>
             </div>
